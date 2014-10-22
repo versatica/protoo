3d48dc3557d8b340db31e2e3b057baf2324e4ff5
Iñaki Baz Castillo <ibc@aliax.net>
2014-10-22 18:44:47 +0200

    qwe

--- test/protoo_test.js
+++ test/protoo_test.js
@@ -4,6 +4,7 @@
 var protoo = require('../');
 var pkg = require('../package.json');
 var http = require('http');
+var url = require('url');
 var WebSocket = require('ws');
 var debug = require('debug')('test');
 
@@ -23,8 +24,41 @@ function runProtooServer(done) {
 		done();
 	});
 
+	global.app.on('error', function(error) {
+		debug(error);
+	});
+
 	global.app.on('ws:connecting', function(connectingInfo, acceptCb, rejectCb, waitCb) {  // jshint ignore:line
-		acceptCb('testuser', 'testuuid');
+		var u = url.parse(connectingInfo.req.url, true);
+		var test = u.query.test;
+		var uuid = 'abcd-1234';
+
+		switch(test) {
+			case 'sync_accept':
+				acceptCb(test, uuid);
+				break;
+
+			case 'sync_reject':
+				rejectCb(403, test);
+				break;
+
+			case 'async_accept':
+				waitCb();
+				process.nextTick(function() {
+					acceptCb(test, uuid);
+				});
+				break;
+
+			case 'async_reject':
+				waitCb();
+				process.nextTick(function() {
+					rejectCb(403, test);
+				});
+				break;
+
+			case 'no_cb_called':
+				break;
+		}
 	});
 }
 
@@ -38,10 +72,8 @@ function stopProtooServer(done) {
 }
 
 
-function wsConnect(username, uuid, protocol) {
-	var ws = new WebSocket('ws://127.0.0.1:54321/?username=' + username + '&uuid=' + uuid, {protocol: protocol});
-
-	return ws;
+function wsConnect(test, protocol) {
+	return new WebSocket('ws://127.0.0.1:54321?test=' + test, {protocol: protocol});
 }
 
 
@@ -60,10 +92,10 @@ exports['test Protoo server'] = {
 		stopProtooServer(done);
 	},
 
-	'fail to connect if WS "protoo" sub-protocol is not given': function(test) {
+	'peer fails to connect if WS "protoo" sub-protocol is not given': function(test) {
 		test.expect(1);
 
-		var ws = wsConnect('testuser', 'testuuid');
+		var ws = wsConnect('fail');
 
 		ws.on('open', function() {
 			test.ok(false);
@@ -77,10 +109,10 @@ exports['test Protoo server'] = {
 		});
 	},
 
-	'connect if WS "protoo" sub-protocol is given': function(test) {
+	'peer sync accept': function(test) {
 		test.expect(1);
 
-		var ws = wsConnect('testuser', 'testuuid', 'protoo');
+		var ws = wsConnect('sync_accept', 'protoo');
 
 		ws.on('open', function() {
 			test.ok(true);
@@ -91,6 +123,78 @@ exports['test Protoo server'] = {
 		ws.on('error', function(error) {
 			debug(error.message);
 			test.ok(false);
+			test.done();
+		});
+	},
+
+	'peer sync reject': function(test) {
+		test.expect(1);
+
+		var ws = wsConnect('sync_reject', 'protoo');
+
+		ws.on('open', function() {
+			test.ok(false);
+			ws.close();
+			test.done();
+		});
+
+		ws.on('error', function(error) {
+			debug(error.message);
+			test.ok(true);
+			test.done();
+		});
+	},
+
+	'peer async accept': function(test) {
+		test.expect(1);
+
+		var ws = wsConnect('async_accept', 'protoo');
+
+		ws.on('open', function() {
+			test.ok(true);
+			ws.close();
+			test.done();
+		});
+
+		ws.on('error', function(error) {
+			debug(error.message);
+			test.ok(false);
+			test.done();
+		});
+	},
+
+	'peer async reject': function(test) {
+		test.expect(1);
+
+		var ws = wsConnect('async_reject', 'protoo');
+
+		ws.on('open', function() {
+			test.ok(false);
+			ws.close();
+			test.done();
+		});
+
+		ws.on('error', function(error) {
+			debug(error.message);
+			test.ok(true);
+			test.done();
+		});
+	},
+
+	'peer fails to connect if no callback is called on "ws:connecting"': function(test) {
+		test.expect(1);
+
+		var ws = wsConnect('no_cb_called', 'protoo');
+
+		ws.on('open', function() {
+			test.ok(false);
+			ws.close();
+			test.done();
+		});
+
+		ws.on('error', function(error) {
+			debug(error.message);
+			test.ok(true);
 			test.done();
 		});
 	}
